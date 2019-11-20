c25baf9faf3663e2a0ecccdab94c2aec7653310b
mehrabrafi143 <maharabrafi@gmail.com>
2019-11-20 11:44:22 +0600

    Updates

---
+++ precache-manifest.de6828d5fb867b2a33492eea7632bd66.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd256559ee1365a36d7dc14343cd179",
+    "url": "/bkash-app/index.html"
+  },
+  {
+    "revision": "5e0c3ac4945653189616",
+    "url": "/bkash-app/static/css/2.3e14d6f1.chunk.css"
+  },
+  {
+    "revision": "cd226b85150a21702fcc",
+    "url": "/bkash-app/static/css/main.7ff5a3c1.chunk.css"
+  },
+  {
+    "revision": "5e0c3ac4945653189616",
+    "url": "/bkash-app/static/js/2.232de831.chunk.js"
+  },
+  {
+    "revision": "cd226b85150a21702fcc",
+    "url": "/bkash-app/static/js/main.9a439782.chunk.js"
+  },
+  {
+    "revision": "29e345115d513df4b0e3",
+    "url": "/bkash-app/static/js/runtime-main.8f5579c8.js"
+  },
+  {
+    "revision": "b5f018eeb29ada62ef5b15dcfa885428",
+    "url": "/bkash-app/static/media/data-soft.b5f018ee.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/bkash-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/bkash-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/bkash-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/bkash-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/bkash-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b5afe1de67fdebbade3cc7e74b7a30fd",
+    "url": "/bkash-app/static/media/ipadpro1.b5afe1de.png"
+  },
+  {
+    "revision": "c9fbe651556d12ffa9c860d9d291e5dc",
+    "url": "/bkash-app/static/media/login.c9fbe651.png"
+  },
+  {
+    "revision": "84bc0f3369bdd173166873ecd0200d61",
+    "url": "/bkash-app/static/media/man.84bc0f33.jpg"
+  }
+]);
